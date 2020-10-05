10957306b9f720461b0256446d054c3413aa85f4
Victor Koji <vitorkoji15@gmail.com>
2020-10-05 19:56:55 -0400

    Corrigindo api

--- spotify-api/src/components/Home.vue
+++ spotify-api/src/components/Home.vue
@@ -10,7 +10,18 @@ export default {
   name: 'Home',
   props: {
     msg: String
-  }
+  },
+  methods: {
+      getHashParams() {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+            q = window.location.hash.substring(1);
+        while ( e = r.exec(q)) {
+            hashParams[e[1]] = decodeURIComponent(e[2]);
+        }
+        return hashParams;
+      }
+    }
 }
 </script>
 
