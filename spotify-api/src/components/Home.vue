f0843b6c5fb31d1dec5544eb0feade2c5afc4d8e
Victor Koji <vitorkoji15@gmail.com>
2020-10-04 22:54:21 -0400

    Vue criado

---
+++ spotify-api/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <h1>{{ msg }}</h1>
+    <a href="http://localhost:8888" class="btn btn-primary">Log in with Spotify</a>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+
+</style>
